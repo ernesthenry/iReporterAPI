bc0356ebcbb181a9ef82f1872d63507cbd893c25
ernesthenry <henry38ernest@gmail.com>
2019-01-17 18:16:15 +0300

    Added PEP8 style

--- api/models/red_flag_incident.py
+++ api/models/red_flag_incident.py
@@ -4,19 +4,19 @@ my_red_flags = []
 
 class Redflag:
     """ model class for red-flags """
-    def __init__(self, createdBy, _type, place, status, Images, Videos, comment):
+    def __init__(self, **kwargs):
         self._id = len(my_red_flags) + 1
         self.createdOn = datetime.now()
-        self.createdBy = createdBy
-        self.type = _type
-        self.location = place
-        self.status = status
-        self.Images = Images
-        self.Videos = Videos
-        self.comment = comment
+        self.createdBy = kwargs["createdBy"]
+        self.type = kwargs["_type"]
+        self.location = kwargs["place"]
+        self.status = kwargs["status"]
+        self.Images = kwargs["Images"]
+        self.Videos = kwargs["Videos"]
+        self.comment = kwargs["comment"]
 
     def format_record(self):
-        """ Method that returns data in json format """
+        """ Method that returns dictionary representation of the object """
         return {
             "id": self._id,
             "createdOn": self.createdOn,
--- api/models/user_incident.py
+++ api/models/user_incident.py
@@ -1,21 +1,20 @@
+from datetime import datetime
 
 user_data = []
 
 
 class User:
     """ model class for users """
-    def __init__(
-        self, firstname, lastname, othernames, email, phone_number, username, registered, is_admin
-        ):
+    def __init__(self, **kwargs):
         self._id = len(user_data)+1
-        self.firstname = firstname
-        self.lastname = lastname
-        self.othernames = othernames
-        self.email = email
-        self.phone_number = phone_number
-        self.username = username
-        self.registered = registered
-        self.is_admin = is_admin
+        self.firstname = kwargs["firstname"]
+        self.lastname = kwargs["lastname"]
+        self.othernames = kwargs["othernames"]
+        self.email = kwargs["email"]
+        self.phone_number = kwargs["phone_number"]
+        self.username = kwargs["username"]
+        self.registered = datetime.now()
+        self.is_admin = False
 
     def format_user_record(self):
         return {
