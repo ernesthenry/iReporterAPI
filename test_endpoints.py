9555440fef3a961365f1b5cadf462717af1e8ff9
ernesthenry <henry38ernest@gmail.com>
2019-01-17 16:22:00 +0300

    [Deliver #163287223] Removed unnecessary spaces

--- test_endpoints.py
+++ test_endpoints.py
@@ -2,12 +2,9 @@ import unittest
 import json
 from api.views.red_flag_routes import app, my_red_flags
 
-
 class BaseTest(unittest.TestCase):
 	def setUp(self):
 		self.client = app.test_client()
-
-
 		self.sample_record_data = {
             "createdBy": "anthony",
             "type": "red-flag",
@@ -25,20 +22,18 @@ class TestRedFlag(BaseTest):
 			data = json.dumps(self.sample_record_data),
 			content_type = "application/json"
 			)
-		
 		self.assertEqual(response.status_code, 201)
 		self.assertEqual(response.content_type, "application/json")
 		self.assertIn(b"Created red-flag record", response.data)
-
-
-		
+	
 	def test_home(self):
+		""" Testing for  data at the home route """
 		response = self.client.get('/')
 		assert b'Welcome to ernest\'s iReporter app.' in response.data
 		assert response.status_code == 200
 
-
 	def test_get_all_redflags(self):
+		""" Test for getting all red-flag records"""
 		self.client.post(
 			"/api/v1/red-flags",
 			data = json.dumps(self.sample_record_data),
@@ -51,7 +46,6 @@ class TestRedFlag(BaseTest):
 		self.assertEqual(response.status_code, 200)
 		self.assertIn("createdBy", str(response.data))
 		self.assertEqual(response.content_type, "application/json")
-
 
 	""" missing value field = bad """
 	def test_post(self):
@@ -69,8 +63,8 @@ class TestRedFlag(BaseTest):
 		self.assertEqual(response.status_code, 400)
 		self.assertEqual("Required field is missing", response_data['Error'])
 
-
 	def test_fetch_specific_redflag(self):
+		""" Test for getting a specific red-flag record """
 		response = self.client.get(
 			'/api/v1/red-flags/1',
 			content_type = "application/json")
@@ -83,14 +77,13 @@ class TestRedFlag(BaseTest):
 			content_type = "application/json")
 		assert response.status_code == 404
 
-
 	def test_edit_location_and_comment(self):
+		""" Test to edit location and  comment  of a red-flag record"""
 		self.client.post(
 			"/api/v1/red-flags",
 			data = json.dumps(self.sample_record_data),
 			content_type = "application/json"
 			)
-
 		new_location = {"location": "Kampala"}
 		response = self.client.patch(
 			"/api/v1/red-flags/1/location",
@@ -117,9 +110,8 @@ class TestRedFlag(BaseTest):
 		self.assertIn("Updated red-flag's record comment", response_data["message"])
 		self.assertIsInstance(response_data, dict)
 
-
-
 	def test_delete_record(self):
+		""" Test to delete a specific red-flag record """
 		self.client.post(
 			"/api/v1/red-flags",
 			data = json.dumps(self.sample_record_data),
